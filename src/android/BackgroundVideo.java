003fce59d79e23a99d890965bf77160c593cfede
mbw13 <mbw13@mbw13-no-MacBook.local>
2018-09-13 14:44:58 +0900

    アンドロイド側のパラメーターの取得を修正

--- src/android/BackgroundVideo.java
+++ src/android/BackgroundVideo.java
@@ -99,9 +99,9 @@ public class BackgroundVideo extends CordovaPlugin {
     }
 
     private void Start(JSONArray args) throws JSONException {
-        // params fileStorage, filename, camera, quality
-        final String filename = args.getString(1);
-        final String cameraFace = args.getString(2);
+        // params filename, camera
+        final String filename = args.getString(0);
+        final String cameraFace = args.getString(1);
 
         if (videoOverlay == null) {
             videoOverlay = new VideoOverlay(cordova.getActivity()); //, getFilePath());
